28cc18393308ef02df7c6bce9d6a0647c49cd2bd
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-14 18:51:06 +0100

    Button.vue added

---
+++ src/components/Button.vue
@@ -0,0 +1,105 @@
+<template lang="pug">
+  .button(:class="classType")
+    .link-content-container(v-if="linkContent")
+      .link-image(:style="{ backgroundImage: `url(${linkContent.imgSrc})` }")
+      .link-content.d-flex.align-items-end.justify-content-between
+        .link-text
+          .link-title {{ linkContent.title }}
+          .link-description {{ linkContent.description }}
+        .link-image-link
+          img(:src="linkImg")
+    p(v-else) {{ title }}
+</template>
+
+<script>
+import linkImg from '../assets/link.png'
+
+export default {
+  name: "Button",
+  props: {
+    classType: String,
+    linkContent: Object,
+    title: String
+  },
+  data() {
+    return {
+      linkImg
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    font-size: 18px;
+    border: 1px solid transparent;
+    width: fit-content;
+    margin: 4px;
+
+    &.action {
+      background: $paradise;
+      color: white;
+      cursor: pointer;
+      font-weight: 600;
+      border-radius: 50px;
+      padding: 9px 24px;
+
+      &:hover {
+        background: $ocean;
+        transition: .25s;
+        color: white;
+      }
+    }
+
+    &.link {
+      cursor: pointer;
+      .link-content-container {
+        min-width: 400px;
+        max-width: 400px;
+        min-height: 250px;
+        border-radius: 25px;
+        overflow: hidden;
+        background: black;
+        position: relative;
+        transition: .25s;
+
+        .link-image {
+          max-width: 400px;
+          min-height: 250px;
+          background-size: cover;
+          background-position: center center;
+          opacity: .70;
+        }
+
+        .link-content {
+          position: absolute;
+          bottom: 0;
+          padding: 20px;
+          min-width: 400px;
+          max-width: 400px;
+          max-height: 150px;
+          text-align: left;
+
+          .link-image-link {
+            img {
+              width: 25px;
+            }
+            overflow: hidden;
+          }
+          .link-title {
+            font-size: 24px;
+            color: white;
+            font-weight: 900;
+          }
+          .link-description {
+            color: white;
+          }
+        }
+      }
+    }
+
+    p {
+      margin: 0;
+    }
+  }
+</style>
