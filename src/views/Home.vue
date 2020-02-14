3eeaf037e4114388df0ed21fc39d0d0322b07eac
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-14 18:50:00 +0100

    Home update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,31 @@
 <template lang="pug">
 #Home
-  Loader
+  Loader(v-if="showLoader")
+  Timeline
 </template>
 
 <script>
 import Loader from '../components/Loader.vue'
+import Timeline from '../components/Timeline.vue'
+import botpng from '../assets/bot.png'
 
 export default {
   name: 'Home',
   components: {
-    Loader
+    Loader,
+    Timeline
   },
+  data() {
+    return {
+      botpng,
+      showLoader: false
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showLoader = false
+    }, 10000)
+  }
 }
 </script>
 
@@ -18,6 +33,10 @@ export default {
 #Home {
   img {
     max-width: 400px;
+  }
+  #logo {
+    max-width: 75px;
+    margin-bottom: 25px;
   }
 }
 </style>
