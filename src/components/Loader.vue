e5cacc0ee1554a81214c29c5d09c581ec4b2945f
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-14 18:51:00 +0100

    Loader update

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
-  #Loader.d-flex.flex-column.align-items-center.justify-content-center
-    .loader-container
-      .loader-bar
-    img#botLoaderImage(:src="botpng")
-    .description Basile est un assistant intelligent qui répondra à toutes vos questions concernant la création d'entreprise.
+  #Loader(:class="{ 'disappear' : isMoving }")
+    .loader-container-full.d-flex.flex-column.align-items-center.justify-content-center
+      .loader-container-image
+        img#botLoaderImage(:src="botpng")
+      .loader-infos
+        .description.loader-description-appear Basile est un assistant intelligent qui répondra à toutes vos questions concernant la création d'entreprise.
+        .loader-container
+          .loader-bar
 </template>
 
 <script>
@@ -13,8 +16,14 @@ export default {
   name: "Loader",
   data() {
     return {
-      botpng
+      botpng,
+      isMoving: false
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isMoving = true
+    }, 10000)
   }
 }
 </script>
@@ -23,39 +32,106 @@ export default {
 #Loader {
   width: 100vw;
   height: 100vh;
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10000;
+  background: white;
+  overflow: hidden;
 
-  #botLoaderImage {
-    max-width: 75px;
-    animation-name: jump;
-    animation-timing-function: ease;
+  &.disappear {
+    -webkit-animation: disappear 2s forwards; /* Safari 4+ */
+    -moz-animation:    disappear 2s forwards; /* Fx 5+ */
+    -o-animation:      disappear 2s forwards; /* Opera 12+ */
+    animation:         disappear 2s forwards; /* IE 10+, Fx 29+ */
   }
 
-  .loader-container {
-    width: 100%;
-    height: 10px;
-    position: absolute;
-    top: 0px;
+  .loader-container-full{
+    width: 100vw;
+    height: 100vh;
+    position: relative;
 
-    .loader-bar {
-      height: 10px;
-      background: $ocean;
-      -webkit-animation: loading 4s forwards; /* Safari 4+ */
-      -moz-animation:    loading 4s forwards; /* Fx 5+ */
-      -o-animation:      loading 4s forwards; /* Opera 12+ */
-      animation:         loading 4s forwards; /* IE 10+, Fx 29+ */
+    .loader-container-image{
+      width: 100%;
+      height: 75px;
+      position: relative;
+      margin-bottom: 25px;
+
+      #botLoaderImage {
+        max-width: 75px;
+      }
+    }
+
+    &.remove {
+      display: none!important;
+    }
+
+    .loader-infos {
+      max-width: 600px;
+      -webkit-animation: reduce 7s forwards; /* Safari 4+ */
+      -moz-animation:    reduce 7s forwards; /* Fx 5+ */
+      -o-animation:      reduce 7s forwards; /* Opera 12+ */
+      animation:         reduce 7s forwards; /* IE 10+, Fx 29+ */
+      overflow: hidden;
+      height: 100px;
+
+      .loader-description-appear {
+        margin: 25px auto;
+        -webkit-animation: appear 2s forwards; /* Safari 4+ */
+        -moz-animation:    appear 2s forwards; /* Fx 5+ */
+        -o-animation:      appear 2s forwards; /* Opera 12+ */
+        animation:         appear 2s forwards; /* IE 10+, Fx 29+ */
+      }
+
+      .loader-container {
+        height: 10px;
+        margin: 25px auto;
+        max-width: 200px;
+
+        .loader-bar {
+          height: 2px;
+          border-radius:50px;
+          background: $gradient;
+          -webkit-animation: loading 4s forwards; /* Safari 4+ */
+          -moz-animation:    loading 4s forwards; /* Fx 5+ */
+          -o-animation:      loading 4s forwards; /* Opera 12+ */
+          animation:         loading 4s forwards; /* IE 10+, Fx 29+ */
+        }
+      }
     }
   }
 }
 
-@keyframes jump {
-    0%   { transform: scale(1,1)    translateY(0); }
-    10%  { transform: scale(1.1,.9) translateY(0); }
-    30%  { transform: scale(.9,1.1) translateY(-100px); }
-    50%  { transform: scale(1,1)    translateY(0); }
-    57%  { transform: scale(1,1)    translateY(-7px); }
-    64%  { transform: scale(1,1)    translateY(0); }
-    100% { transform: scale(1,1)    translateY(0); }
+@keyframes appear {
+    0%   {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+      margin: 0;
+    }
+}
+
+@keyframes disappear {
+    0%   {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+}
+
+@keyframes reduce {
+    0%   {
+      height: 100px;
+    }
+    80% {
+      height: 100px;
+    }
+    100% {
+      height: 0px;
+      display: none;
+    }
 }
 
 @keyframes loading {
@@ -70,7 +146,6 @@ export default {
   }
   80% {
     width: 76%;
-    height: 10px;
   }
   100% {
     width: 100%;
